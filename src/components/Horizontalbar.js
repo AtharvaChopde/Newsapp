43b02c14673c03e62f7f888c89641a4be5e76e04
atharvachopde <atharvachopde@gmail.com>
2023-06-25 18:02:12 +0530

    First commit

---
+++ src/components/Horizontalbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./hori.css"
+import Button from './Button'
+
+export default function Horizontalbar() {
+  return (
+    <div class="horizontal-bar">
+    <h1><a href="/" className='anchor'>News.co</a></h1>
+    <div className="btns">
+      <Button/>
+      <Button/>
+      <Button/>
+    
+    
+    </div>
+</div>
+  )
+}
---
+++ src/components/NewsItem.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./Newsitem.css"
+import "./main.css"
+export default function NewsItem(props) {
+  return (
+    <div>
+
+<div class="card">
+  
+  <div className="photo">
+  <img src={props.photo} alt="Avatar" style={{width:"90%",}} />
+  </div>
+  <div class="container">
+    <h4><b>{props.title.slice(0,5)}</b></h4>
+    <p>{props.desc.slice(0,70)}</p>
+  </div>
+
+</div>
+
+
+
+    
+
+
+    </div>
+  )
+}
